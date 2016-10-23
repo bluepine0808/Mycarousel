92953c1bd49b0c3337e31cab13dc39d105ef0b6a
ronny <rongwang.wang@gmail.com>
2016-10-23 14:43:56 +0800

    init code
    Signed-off-by: ronny <rongwang.wang@gmail.com>

---
+++ app/src/main/java/com/bluer/ronny/mycarousel/CarouselRecyclerView.java
@@ -0,0 +1,15 @@
+package com.bluer.ronny.mycarousel;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by ronny on 16-10-22.
+ */
+public class CarouselRecyclerView extends RecyclerView {
+    public CarouselRecyclerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
